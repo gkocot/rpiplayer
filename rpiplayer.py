8474179d2b9ce7b6efdf8d6bb4fd5ab4c7cdf373
gkocot <grzegorz.kocot@gmail.com>
2014-11-24 00:55:24 +0100

    Scrolling work in progress.

--- rpiplayer.py
+++ rpiplayer.py
@@ -23,7 +23,7 @@ framebuffer = [
 	''
 ]
 
-fb_pos = [0, 0]
+fb_pos = 0
 
 lcd = CharLCD(pin_rs=11, pin_rw=7, pin_e=12, pins_data=[13, 15, 16, 18], cols=16, rows=2)
 
@@ -39,11 +39,18 @@ def write_to_lcd():
 
 def scroll_lcd():
 	threading.Timer(1, scroll_lcd).start()
-	str = framebuffer[1][fb_pos[1]:fb_pos[1]+16].ljust(16)
 	lcd.cursor_pos = (1, 0)
-	lcd.write_string(str)
-	#if (framebuffer[1].len() > 16):
-	#	fb_pos[1] = (fb_pos[1] + 1) % framebuffer[1].len()
+	if (len(framebuffer[1]) > 16):
+		global fb_pos
+		pos1 = fb_pos
+		pos2 = (fb_pos + 16) % len(framebuffer[1])
+		str1 = framebuffer[1][pos1 : pos1 + 16]
+		str2 = framebuffer[1][pos2 : pos2 + 16]
+		str = str1 + ' ' + str2
+		fb_pos = (fb_pos + 1) % len(framebuffer[1])
+	else:
+		str = framebuffer[1].ljust(16)[:16]	
+	lcd.write_string(str[:16])
 	
 
 framebuffer[1] = subprocess.Popen(['mpc', 'current'], stdout=subprocess.PIPE).communicate()[0].splitlines()[0]
@@ -58,13 +65,13 @@ while True:
 	if GPIO.event_detected(sw3):
 		p = subprocess.Popen(['mpc', 'prev'], stdout=subprocess.PIPE)
 		framebuffer[1] = p.communicate()[0].splitlines()[0].decode('windows-1252').encode('ascii', 'ignore')
-		fb_pos[1] = 0
+		fb_pos = 0
 		#scroll_lcd()
 		#write_to_lcd()
 	if GPIO.event_detected(sw4):
 		p = subprocess.Popen(['mpc', 'next'], stdout=subprocess.PIPE)
 		framebuffer[1] = p.communicate()[0].splitlines()[0].decode('windows-1252').encode('ascii', 'ignore')
-		fb_pos[1] = 0
+		fb_pos = 0
 		#scroll_lcd()
 		#write_to_lcd()
 
