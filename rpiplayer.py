c2cf30bf4f87b950ac8375798fef5ea7f1dec0fb
gkocot <grzegorz.kocot@gmail.com>
2014-12-13 22:34:56 +0100

    Reorganized code to react on keyboard events in separate thread instead in the main program loop.

--- rpiplayer.py
+++ rpiplayer.py
@@ -18,19 +18,32 @@ GPIO.add_event_detect(sw2, GPIO.FALLING, bouncetime=200)
 GPIO.add_event_detect(sw3, GPIO.FALLING, bouncetime=200)
 GPIO.add_event_detect(sw4, GPIO.FALLING, bouncetime=200)
 #GPIO.output(led, GPIO.HIGH)
-
-framebuffer = [
-	'',
-	''
-]
-
-fb_pos = 1
-
 lcd = CharLCD(pin_rs=11, pin_rw=7, pin_e=12, pins_data=[13, 15, 16, 18], cols=16, rows=2)
 
+framebuffer = ['','']
+fb_pos = [1, 1]
+current_playlist_no=0
+playlists = subprocess.Popen(['mpc', 'lsplaylists'], stdout=subprocess.PIPE).communicate()[0].splitlines()
+random=False
+subprocess.Popen(['mpc', 'random', 'off']).wait()
+subprocess.Popen(['mpc', 'play', '1']).wait()
+
 def display():
-	threading.Timer(1, display).start()
+	global framebuffer
+	global fb_pos
+	for line_no in range(0, 2):
+		str = framebuffer[line_no]
+		if (len(str) > 16):
+			str_display = (' ' + str)[fb_pos[line_no] : fb_pos[line_no] + 16] + (' ' + str[:16])
+			fb_pos[line_no] = (fb_pos[line_no] + 1) % (len(str) + 1)
+		else:
+			str_display = str.ljust(16)
 	
+		lcd.cursor_pos = (line_no, 0)
+		lcd.write_string(str_display[:16])
+
+	threading.Timer(1, display).start()
+'''	
 	lcd.cursor_pos = (0, 0)
 	str = framebuffer[0][:15].ljust(15) + ('*' if random else ' ')
 	lcd.write_string(str)
@@ -56,7 +69,9 @@ def display():
 		str = framebuffer[1].ljust(16)
 	
 	lcd.write_string(str[:16])
-
+	threading.Timer(1, display).start()
+'''
+		
 def get_ip_addr():
 	ip = subprocess.Popen(['ip', 'addr'], stdout=subprocess.PIPE)
 	grep1 = subprocess.Popen(['grep', 'inet.*brd', '-o'], stdout=subprocess.PIPE, stdin=ip.stdout)
@@ -64,12 +79,6 @@ def get_ip_addr():
 	ip.stdout.close()
 	grep1.stdout.close()
 	return grep2.communicate()[0]
-
-current_playlist_no=0
-playlists = subprocess.Popen(['mpc', 'lsplaylists'], stdout=subprocess.PIPE).communicate()[0].splitlines()
-random=False
-subprocess.Popen(['mpc', 'random', 'off']).wait()
-subprocess.Popen(['mpc', 'play', '1']).wait()
 
 # This should recover if LAN is down but is working not reliably enough.
 #def wlan0_watchdog():	
@@ -87,11 +96,54 @@ subprocess.Popen(['mpc', 'play', '1']).wait()
 #	else:
 #		threading.Timer(3, wlan0_watchdog).start()
 
+def keyboard():
+	global framebuffer
+	global fb_pos
+	global random
+	global current_playlist_no
+	global playlists
+	if GPIO.event_detected(sw1):
+		random = not random
+		if random:
+			subprocess.Popen(['mpc', 'random', 'on']).wait()
+			framebuffer[0] = playlists[current_playlist_no][:15].ljust(15) + '*'
+		else:
+			subprocess.Popen(['mpc', 'random', 'off']).wait()
+			framebuffer[0] = playlists[current_playlist_no][:15].ljust(15) + ' '
+
+	if GPIO.event_detected(sw2):
+		current_playlist_no = (current_playlist_no + 1) % len(playlists)
+		subprocess.Popen(['mpc', 'clear']).wait()
+		subprocess.Popen(['mpc', 'load', playlists[current_playlist_no]]).wait()
+		subprocess.Popen(['mpc', 'play', '1']).wait()
+		framebuffer[0] = playlists[current_playlist_no][:15].ljust(15) + ('*' if random else ' ')
+
+	if GPIO.event_detected(sw3):
+		subprocess.Popen(['mpc', 'prev']).wait()
+	if GPIO.event_detected(sw4):
+		subprocess.Popen(['mpc', 'next']).wait()
+	
+	p = subprocess.Popen(['mpc', 'current'], stdout=subprocess.PIPE)
+	current = p.communicate()[0]
+	if current:
+		current = current.splitlines()[0].decode('windows-1252').encode('ascii', 'ignore')
+	else:
+		current = ''
+	# TBD: Is None == ''?
+	
+	if current != framebuffer[1]:
+		framebuffer[1] = current
+		fb_pos[1] = 1
+
+	threading.Timer(1, keyboard).start()
+
 framebuffer[0] = get_ip_addr()
+keyboard()
 display()
 #wlan0_watchdog()
 
 while True:
+	'''
 	if GPIO.event_detected(sw1):
 		random = not random
 		if random:
@@ -108,4 +160,5 @@ while True:
 		subprocess.Popen(['mpc', 'prev']).wait()
 	if GPIO.event_detected(sw4):
 		subprocess.Popen(['mpc', 'next']).wait()
+	'''
 
