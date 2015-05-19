a3cdd0487242a001c0c54262f537efa723a1401a
gkocot <grzegorz.kocot@gmail.com>
2015-05-19 20:21:40 +0200

    Added screen that displays IP address.

--- rpiplayer.py
+++ rpiplayer.py
@@ -1,8 +1,16 @@
+import subprocess
+import time
 import RPi.GPIO as GPIO
 from RPLCD import CharLCD
-import subprocess
-import threading
-import time
+
+
+def get_ip_addr():
+	ip = subprocess.Popen(['ip', 'addr'], stdout=subprocess.PIPE)
+	grep1 = subprocess.Popen(['grep', 'inet.*brd', '-o'], stdout=subprocess.PIPE, stdin=ip.stdout)
+	grep2 = subprocess.Popen(['grep', '-E', '([0-9]{1,3}\.){3}[0-9]{1,3}', '-o'], stdout=subprocess.PIPE, stdin=grep1.stdout)
+	ip.stdout.close()
+	grep1.stdout.close()
+	return grep2.communicate()[0].rstrip()
 
 
 class MPD(object):
@@ -194,7 +202,7 @@ class PlayerScreen(Screen):
 		if (len(str) > LCD.COLS):
 			return ' '.join([str, str])[self.pos[row]:self.pos[row] + LCD.COLS]
 		else:
-			return str.ljust(LCD.COLS)
+			return str
 
 	def open(self):
 		self.pos = [0, 0]
@@ -223,10 +231,24 @@ class PlayerScreen(Screen):
 			self.time = time.time()
 
 
+class ShowIPScreen(Screen):
+	def __init__(self):
+		self.ip = ''
+
+	def get_text(self, row):
+		if  row == 0:
+			return 'IP Address'
+		elif row == 1:
+			return self.ip
+
+	def open(self):
+		self.ip = get_ip_addr()
+
+
 class DispMgr(object):
 	screen = [
 		PlayerScreen(),
-		StaticScreen(["RPi Player", "Version 0.1"]),
+		ShowIPScreen(),
 	]
 
 	current_screen = 0
@@ -234,8 +256,8 @@ class DispMgr(object):
 	@staticmethod
 	def refresh():
 		DispMgr.screen[DispMgr.current_screen].refresh()
-		LCD.write(0, 0, DispMgr.screen[DispMgr.current_screen].get_text(0))
-		LCD.write(1, 0, DispMgr.screen[DispMgr.current_screen].get_text(1))
+		LCD.write(0, 0, DispMgr.screen[DispMgr.current_screen].get_text(0).ljust(LCD.COLS)[:LCD.COLS])
+		LCD.write(1, 0, DispMgr.screen[DispMgr.current_screen].get_text(1).ljust(LCD.COLS)[:LCD.COLS])
 		
 	@staticmethod
 	def key_hold(id):		
@@ -256,142 +278,3 @@ Keyboard.init()
 while True:
 	Keyboard.process_keys()
 	DispMgr.refresh()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-fb = ['', '']
-fb_pos = [1, 1]
-current_playlist_no=0
-playlists = subprocess.Popen(['mpc', 'lsplaylists'], stdout=subprocess.PIPE).communicate()[0].splitlines()
-random=False
-subprocess.Popen(['mpc', 'random', 'off']).wait()
-
-def get_ip_addr():
-	ip = subprocess.Popen(['ip', 'addr'], stdout=subprocess.PIPE)
-	grep1 = subprocess.Popen(['grep', 'inet.*brd', '-o'], stdout=subprocess.PIPE, stdin=ip.stdout)
-	grep2 = subprocess.Popen(['grep', '-E', '([0-9]{1,3}\.){3}[0-9]{1,3}', '-o'], stdout=subprocess.PIPE, stdin=grep1.stdout)
-	ip.stdout.close()
-	grep1.stdout.close()
-	return grep2.communicate()[0]
-
-def display():
-	global fb
-	global fb_pos
-
-	for line_no in range(0, 2):
-		str = fb[line_no]
-		if (len(str) > 16):
-			str_display = (' ' + str)[fb_pos[line_no] : fb_pos[line_no] + 16] + (' ' + str[:16])
-			fb_pos[line_no] = (fb_pos[line_no] + 1) % (len(str) + 1)
-		else:
-			str_display = str.ljust(16)
-	
-		lcd.cursor_pos = (line_no, 0)
-		lcd.write_string(str_display[:16])
-
-	threading.Timer(1, display).start()
-
-def keyboard():
-	global fb
-	global fb_pos
-	global random
-	global current_playlist_no
-	global playlists
-
-	if GPIO.event_detected(sw1):
-		random = not random
-		if random:
-			subprocess.Popen(['mpc', 'random', 'on']).wait()
-			fb[0] = playlists[current_playlist_no][:15].ljust(15) + '*'
-		else:
-			subprocess.Popen(['mpc', 'random', 'off']).wait()
-			fb[0] = playlists[current_playlist_no][:15].ljust(15) + ' '
-
-	if GPIO.event_detected(sw2):
-		current_playlist_no = (current_playlist_no + 1) % len(playlists)
-		subprocess.Popen(['mpc', 'clear']).wait()
-		subprocess.Popen(['mpc', 'load', playlists[current_playlist_no]]).wait()
-		subprocess.Popen(['mpc', 'play', '1']).wait()
-		fb[0] = playlists[current_playlist_no][:15].ljust(15) + ('*' if random else ' ')
-
-	if GPIO.event_detected(sw3):
-		subprocess.Popen(['mpc', 'prev']).wait()
-	if GPIO.event_detected(sw4):
-		subprocess.Popen(['mpc', 'next']).wait()
-	
-	p = subprocess.Popen(['mpc', 'current'], stdout=subprocess.PIPE)
-	current = p.communicate()[0]
-	if current:
-		current = current.splitlines()[0].decode('windows-1252').encode('ascii', 'ignore')
-	
-	if current != fb[1]:
-		fb[1] = current
-		fb_pos[1] = 1
-
-	threading.Timer(1, keyboard).start()
-
-fb[0] = get_ip_addr()
-keyboard()
-display()
-
-while True:
-	ip = get_ip_addr()
-	if not ip:
-		fb[0] = 'network down'
-		subprocess.Popen(['ifdown', 'wlan0']).wait()
-		subprocess.Popen(['ifup', 'wlan0']).wait()
-		ip = get_ip_addr()
-		if ip:
-			fb[0] = ip
-
-	time.sleep(1)
